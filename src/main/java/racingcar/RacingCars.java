9c5da3c219196dbd9ec7041dcc431f3d37a791f2
sunghun <nokdonwithkj211@gmail.com>
2025-10-03 21:10:07 +0900

    feat(RacingCars): 라운드 진행하는 메서드 그리고 우승자 찾는 메서드 구현

--- src/main/java/racingcar/RacingCars.java
+++ src/main/java/racingcar/RacingCars.java
@@ -1,7 +1,9 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCars {
     private final List<Car> racingCars = new ArrayList<>();
@@ -16,5 +18,20 @@ public class RacingCars {
         return new ArrayList<>(racingCars);
     }
 
-    // 각각의 수에 제공해야 하는 상황임
+    // 라운드 진행하기 (한 라운드 당)
+    public void movePerRound() {
+        List<Integer> randomNumbers = RandomNumberGenerator.generateNRandomNumbers(racingCars.size());
+        for(int i = 0 ; i < racingCars.size(); i++) {
+            racingCars.get(i).receiveNumber(randomNumbers.get(i));
+        }
+    }
+
+    // 우승자 찾기
+    public List<Car> findWinner() {
+        int maxDistance = racingCars.stream().map(Car::getDistance).
+                max(Comparator.naturalOrder()).orElseThrow(IllegalArgumentException::new);
+
+        return racingCars.stream().filter(car -> car.getDistance() == maxDistance)
+                .collect(Collectors.toList());
+    }
 }
